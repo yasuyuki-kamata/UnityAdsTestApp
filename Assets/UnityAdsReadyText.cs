042d1fdc9f6cde91f092292283985be2bd990ece
Yasuyuki Kamata <flandre.s199@gmail.com>
2016-06-11 01:01:50 +0900

    Add checkAdsIsReady script & change project settings

---
+++ Assets/UnityAdsReadyText.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.Advertisements;
+
+[RequireComponent(typeof(Text))]
+public class UnityAdsReadyText : MonoBehaviour
+{
+	public Text text;
+
+	void Start ()
+	{
+		if (text == null)
+			text = GetComponent<Text> ();
+	}
+
+	void Update ()
+	{
+		text.text = "isReady?: " + Advertisement.IsReady ().ToString ();
+	}
+}
