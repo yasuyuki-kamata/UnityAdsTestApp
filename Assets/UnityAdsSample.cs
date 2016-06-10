b1f5274785a0c858ed7aba9fdc242ae7aeb53021
Yasuyuki Kamata <flandre.s199@gmail.com>
2016-06-10 21:41:45 +0900

    first commit

---
+++ Assets/UnityAdsSample.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using UnityEngine.Advertisements;
+using UnityEngine.Events;
+
+public class UnityAdsSample : MonoBehaviour
+{
+	public UnityEvent OnUnityAdsFinished;
+	public UnityEvent OnUnityAdsSkipped;
+	public UnityEvent OnUnityAdsFailed;
+	ShowOptions option;
+
+	void Start ()
+	{
+		option = new ShowOptions { resultCallback = HandleShowResult };
+		OnUnityAdsFinished.AddListener(FinishedEvent);
+		OnUnityAdsSkipped.AddListener(SkippedEvent);
+		OnUnityAdsFailed.AddListener(FailedEvent);
+	}
+
+	private void HandleShowResult (ShowResult result)
+	{
+		switch (result)
+		{
+		case ShowResult.Finished:
+			OnUnityAdsFinished.Invoke ();
+			break;
+		case ShowResult.Skipped:
+			OnUnityAdsSkipped.Invoke ();
+			break;
+		case ShowResult.Failed:
+			OnUnityAdsFailed.Invoke ();
+			break;
+		default:
+			break;
+		}
+	}
+
+	public void ShowAd ()
+	{
+		if (Advertisement.IsReady ()) {
+			Debug.Log ("Show!", this);
+			Advertisement.Show ("rewardedVideo", option);
+		}
+	}
+
+	private void FinishedEvent ()
+	{
+		Debug.Log ("Finished!", this);
+	}
+
+	private void SkippedEvent ()
+	{
+		Debug.Log ("Skipped!", this);
+	}
+
+	private void FailedEvent ()
+	{
+		Debug.Log ("Failed!", this);
+	}
+}
---
+++ Assets/UnityVersionText.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class UnityVersionText : MonoBehaviour
+{
+	public Text text;
+	void Start ()
+	{
+		if (text == null)
+			text = GetComponent<Text> ();
+
+		string _text = "";
+		_text += "Built by Unity " + UnityEngine.Application.unityVersion.ToString();
+		_text += "\n";
+		_text += "Unity Ads Version : Built-in";
+		text.text = _text;
+	}
+}
